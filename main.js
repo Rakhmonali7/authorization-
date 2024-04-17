f0b71d0472b2d47bd5edb03408d269dcc27c69c6
Rakhmonali7 <zamonalievr7@gmail.com>
2024-04-17 23:07:12 +0900

    first commit

--- main.js
+++ main.js
@@ -1,20 +1,25 @@
 const express = require('express');
-const {connectToDb, getDb} = require('./db');
+const { connectToDb, getDb } = require('./db');
 
 const app = express();
-let db
-connectToDb((err) => {
-    if(!err){
-        app.listen(3000, () => {
-            console.log('app listening on port 3000');
-        })
-        db = getDb();
-    }
-    
-})
-
-
+let db;
+connectToDb(err => {
+  if (!err) {
+    app.listen(3000, () => {
+      console.log('app listening on port 3000');
+    });
+    db = getDb();
+  }
+});
+connectToDb(err => {
+  if (!err) {
+    app.listen(3000, () => {
+      console.log('app listening on port 3000');
+    });
+    db = getDb();
+  }
+});
 
 app.get('/', (req, res) => {
-    res,json({msg: "welcome the api"})
-    })
+  res, json({ msg: 'welcome the api' });
+});
